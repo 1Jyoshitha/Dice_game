6fd622f7c8be37ddd4c1fc6e65f8669328c04b4b
1Jyoshitha <136741839+1Jyoshitha@users.noreply.github.com>
2025-06-17 13:36:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-document.query
+var a = Math.floor(Math.random()*6+1);
+
+var b = Math.floor(Math.random()*6+1);
+var e = "/images/dice"+a+".png";
+var f = "/images/dice"+b+".png";
+var c = document.querySelector(".img1").setAttribute("src",e);
+var d = document.querySelector(".img2").setAttribute("src",f);
+var head = document.querySelector("h1");
+if(a==b){
+    head.textContent="Draw!";
+}
+else if(a>b){
+    head.textContent="Player 1 Wins!";
+}
+else{
+    head.textContent="Player 2 Wins!";
+}
